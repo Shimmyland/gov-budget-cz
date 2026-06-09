8e17fc9ff26f4ff744d76e66e258fa20d7a53a50
Simon Libiger <116663110+Shimmyland@users.noreply.github.com>
2026-06-09 13:31:35 +0200

    refactor: forget to remove an import

--- app/_db/client.ts
+++ app/_db/client.ts
@@ -1,6 +1,5 @@
 import path from 'node:path'
 
-import { sql } from 'drizzle-orm'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import { logger } from '../lib/logger'
